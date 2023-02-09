9e723394efd301b9a316572c4ec86fdc783586e1
Cesar Santamaria <cesar.santamaria@icloud.com>
2023-02-09 18:45:19 +0100

    Install cors jwt.

---
+++ backend/db/modals/order.js
@@ -0,0 +1,198 @@
+import { query } from "../client";
+
+async function createOrder(userId) {
+  try {
+    const {
+      rows: [order],
+    } = await query(
+      `
+      INSERT INTO orders("userId") 
+      VALUES($1) 
+      RETURNING *;
+    `,
+      [userId]
+    );
+    return order;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getOrderDetails(userId) {
+  try {
+    const { rows } = await query(`
+        SELECT o.id AS order_id, o."orderDate", o."isCheckedOut", o."checkoutDate", o."checkoutSum",
+          oi.quantity, p.id AS "productId", p.title, p.price, p.images[1] AS image, p."stripePrice"
+        FROM orders o
+        JOIN order_items oi ON o.id = oi."orderId"
+        JOIN products p ON oi."productId" = p.id
+        WHERE o."isCheckedOut" = true AND o."userId" = ${userId};
+      `);
+
+    const orders = [];
+    let currentOrderId = null;
+    let currentOrder = null;
+
+    for (let i = 0; i < rows.length; i++) {
+      const row = rows[i];
+      const {
+        order_id,
+        orderDate,
+        isCheckedOut,
+        checkoutDate,
+        checkoutSum,
+        quantity,
+        productId,
+        title,
+        price,
+        image,
+      } = row;
+
+      if (order_id !== currentOrderId) {
+        currentOrder = {
+          order_id,
+          orderDate,
+          isCheckedOut,
+          checkoutDate,
+          checkoutSum,
+          orderItems: [],
+        };
+
+        orders.push(currentOrder);
+        currentOrderId = order_id;
+      }
+      const orderItem = {
+        quantity,
+        productId,
+        title,
+        price,
+        image,
+      };
+
+      currentOrder.orderItems.push(orderItem);
+    }
+
+    return orders;
+  } catch (error) {
+    console.error("Error executing query:", error);
+    throw error;
+  }
+}
+
+async function getCart(userId) {
+  try {
+    const { rows } = await query(
+      `
+        SELECT oi.id AS "orderItemId", o.id AS order_id, o."isCheckedOut",
+          oi.quantity, p.id AS "productId", p.title, p.price, p."stripePrice" ,p.images[1] AS image
+        FROM orders o
+        JOIN order_items oi ON o.id = oi."orderId"
+        JOIN products p ON oi."productId" = p.id
+        WHERE o."isCheckedOut" = false
+        AND o."userId" = $1;
+      `,
+      [userId]
+    );
+    return rows;
+  } catch (error) {
+    console.error("Error executing query:", error);
+    throw error;
+  }
+}
+
+async function getAllOrders() {
+  try {
+    const { rows } = await query(`
+      SELECT * 
+      FROM orders;
+    `);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getOrderById(id) {
+  try {
+    const { rows } = await query(`
+      SELECT * 
+      FROM orders
+      WHERE id=${id};
+    `);
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getOrderByUserId(userId) {
+  try {
+    const { rows } = await query(`
+        SELECT * 
+        FROM orders
+        WHERE "userId"=${userId} AND "isCheckedOut"=false;
+      `);
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+async function getOrderCheckoutByUserId(userId) {
+  try {
+    const { rows } = await query(`
+        SELECT * 
+        FROM orders
+        WHERE "userId"=${userId} AND "isCheckedOut"=true;
+      `);
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateOrders({ id, ...fields }) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+
+  const {
+    rows: [order],
+  } = await query(
+    `
+  UPDATE orders
+  SET ${setString}
+  WHERE id=${id}
+  RETURNING *;
+  `,
+    Object.values(fields)
+  );
+  return order;
+}
+
+async function deleteOrder(id) {
+  await query(`
+  DELETE FROM order_items WHERE "orderId"=${id};
+  `);
+  const {
+    rows: [order],
+  } = await query(`
+  DELETE FROM orders WHERE id=${id}
+  RETURNING *;
+  `);
+  return order;
+}
+
+export default {
+  createOrder,
+  getAllOrders,
+  getOrderByUserId,
+  deleteOrder,
+  getOrderById,
+  updateOrders,
+  getOrderCheckoutByUserId,
+  getOrderDetails,
+  getCart,
+};
