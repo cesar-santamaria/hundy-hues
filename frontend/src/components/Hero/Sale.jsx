cc6a4a087f8f36ad16eb208e64b7bcc6ecbe0644
Cesar Santamaria <cesar.santamaria@icloud.com>
2023-03-10 21:10:00 +0100

    Add Sale component

---
+++ frontend/src/components/Hero/Sale.jsx
@@ -0,0 +1,11 @@
+
+export default function Sale() {
+    return (
+        <article className="discountsContainer">
+            <section className="discountsTop">
+                <h1>Our Features Special For You</h1>
+                <p>We provide a variety of specials features for all of you, to make it easier and make you even more happy shopping here.</p>
+            </section>
+        </article>
+    )
+}
