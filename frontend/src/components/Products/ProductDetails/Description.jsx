e86a4661ebf7ce8a2dcef297b890ea299087fe12
Cesar Santamaria <cesar.santamaria@icloud.com>
2023-03-16 19:10:00 +0100

    Add product description component.

---
+++ frontend/src/components/Products/ProductDetails/Description.jsx
@@ -0,0 +1,10 @@
+
+export default function Description({ description }) {
+    return (
+        <>
+            <section className="moreDetailsList">
+                <p className="otherFont">{description}</p>
+            </section>
+        </>
+    )
+}
