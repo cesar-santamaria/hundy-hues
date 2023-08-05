ccb5a8c69c892e800b674243718dfd23570d0dbc
Cesar Santamaria <cesar.santamaria@icloud.com>
2023-08-05 18:53:59 -0500

    Add styling component.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 const app = require("./app");
 const client = require("./db/client");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const port = 5432;
 
 app.use(morgan("dev"));
+
+// render
+app.use(
+  cors({
+    origin: "",
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 client
   .connect()
